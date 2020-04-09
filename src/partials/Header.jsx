7600e12108f38387d35f7127ef622336351a7f83
jgersain <jhersein@gmail.com>
2020-04-09 17:46:30 -0500

    Create header partial component.

---
+++ src/partials/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = ({ siteTitle }) => (
+  <header className="row-auto col-span-12 bg-blue-600">
+    <h1>{siteTitle}</h1>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+}
+
+Header.defaultProps = {
+  siteTitle: '',
+}
+
+export default Header
