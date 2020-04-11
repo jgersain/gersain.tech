a61444aa0d2292d0bbd666eaff703057a021db0a
jgersain <jhersein@gmail.com>
2020-04-11 04:16:13 -0500

    Add resume button.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <BaseLayout>
     <SEO title="José Gersain" />
+    <a className="bg-transparent hover:bg-teal-600 text-orange-300 hover:text-gray-800 border border-orange-300 hover:border-transparent py-2 px-4 inline-flex items-center rounded" href="">
+      <span className="font-poppins text-xl">Resume</span>
+    </a>
   </BaseLayout>
 )
 
