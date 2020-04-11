637eb5da8cdc28df896ce77784d9118529661d8c
jgersain <jhersein@gmail.com>
2020-04-11 04:31:46 -0500

    Add resume pdf.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import BaseLayout from "../layouts/BaseLayout"
 import SEO from "../components/seo"
+import Resume from '../../content/CV_2019.pdf'
 
 const IndexPage = () => (
   <BaseLayout>
     <SEO title="José Gersain" />
-    <a className="bg-transparent hover:bg-teal-600 text-orange-300 hover:text-gray-800 border border-orange-300 hover:border-transparent py-2 px-4 inline-flex items-center rounded" href="">
+    <a className="bg-transparent hover:bg-teal-600 text-orange-300 hover:text-gray-800 border border-orange-300 hover:border-transparent py-2 px-4 inline-flex items-center rounded" href={Resume} target="_blank">
       <span className="font-poppins text-xl">Resume</span>
     </a>
   </BaseLayout>
