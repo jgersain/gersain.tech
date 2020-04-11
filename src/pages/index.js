7137bd855eadcfee979a689a57a5b64ed9d8b6ad
jgersain <jhersein@gmail.com>
2020-04-11 04:40:07 -0500

    Fix links cross-origin destinations.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Resume from '../../content/CV_2019.pdf'
 const IndexPage = () => (
   <BaseLayout>
     <SEO title="José Gersain" />
-    <a className="bg-transparent hover:bg-teal-600 text-orange-300 hover:text-gray-800 border border-orange-300 hover:border-transparent py-2 px-4 inline-flex items-center rounded" href={Resume} target="_blank">
+    <a className="bg-transparent hover:bg-teal-600 text-orange-300 hover:text-gray-800 border border-orange-300 hover:border-transparent py-2 px-4 inline-flex items-center rounded" href={Resume} target="_blank" rel="noopener">
       <span className="font-poppins text-xl">Resume</span>
     </a>
   </BaseLayout>
--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 export default () => (
   <footer className="col-span-12 bg-gray-900 text-gray-600 py-10 text-center">
     <div className="flex justify-center py-2">
-      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://github.com/jgersain" aria-label="GitHub">
+      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://github.com/jgersain" aria-label="GitHub" rel="noopener">
         <span className="icon-github text-4xl" />
       </a>
-      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://www.linkedin.com/in/gersain/" aria-label="Linkedin">
+      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://www.linkedin.com/in/gersain/" aria-label="Linkedin" rel="noopener">
         <span className="icon-linkedin text-4xl" />
       </a>
-      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://twitter.com/jhersein" aria-label="Twitter">
+      <a className="text-teal-600 hover:text-orange-200 p-3" target="_blank" href="https://twitter.com/jhersein" aria-label="Twitter" rel="noopener">
         <span className="icon-twitter text-4xl" />
       </a>
     </div>
     
     <p className="my-2 tracking-widest text-orange-200">
       Built with {` `}
-      <a href="https://www.gatsbyjs.org" target="_blank">Gatsby</a>
+      <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener">Gatsby</a>
       {` & `}
-      <a href="https://tailwindcss.com/" target="_blank">Tailwindcss</a>
+      <a href="https://tailwindcss.com/" target="_blank" rel="noopener">Tailwindcss</a>
     </p>
     © {new Date().getFullYear()} ― José Gersain
   </footer>
